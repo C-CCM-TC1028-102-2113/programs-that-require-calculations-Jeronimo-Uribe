bc4ac153c29a865660861b708f757585476ba82d
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 12:11:59 -0500

    Update exercise.py

--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -2,7 +2,12 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
-
+#Lustros
+añon=int(input('Dame el año de nacimiento: '))
+añoa=int(input('Dame el año actual: '))
+edad=añoa-añon
+lustro=edad/5
+print('Los lustros que has vivido son: %0.1f' %lustro)
 
 if __name__ == '__main__':
     main()
