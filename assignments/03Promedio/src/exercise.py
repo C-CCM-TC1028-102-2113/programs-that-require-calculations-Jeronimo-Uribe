1f2bbab752fd83578f10b6993983b1754024ba22
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 12:03:45 -0500

    Update exercise.py

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -2,6 +2,13 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+#Promedio 4 materias
+materia1=int(input('Calificación de la materia: '))
+materia2=int(input('Calificación de la materia: '))
+materia3=int(input('Calificación de la materia: '))
+materia4=int(input('Calificación de la materia: '))
+promedio=(materia1+materia2+materia3+materia4)/4
+print('El promedio es: %0.2f' %promedio)
 
 if __name__ == '__main__':
     main()
