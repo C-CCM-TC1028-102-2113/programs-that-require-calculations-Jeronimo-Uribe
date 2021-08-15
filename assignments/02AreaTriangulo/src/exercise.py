b9456ac3ea392a2db1a9aaec9b82ff25ac6ebc31
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 11:59:06 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -2,7 +2,10 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
-
+base=float(input('Dame la base: '))
+altura=float(input('Dame la altura: '))
+area=(base*altura)/2
+print('El área es: %0.1f' %area)
 
 if __name__ == '__main__':
     main()
