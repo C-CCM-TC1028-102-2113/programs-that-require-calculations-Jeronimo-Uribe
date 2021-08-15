e2fa8285c60804cb02cd7f51c0edda5182f0c498
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 12:15:11 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -2,6 +2,13 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+#Pendiente de recta
+x1=float(input('Dame x1: '))
+y1=float(input('Dame y1: '))
+x2=float(input('Dame x2: '))
+y2=float(input('Dame y2: '))
+m=(y2-y1)/(x2-x1)
+print('Pendiente: %0.2f' %m)
 
 if __name__ == '__main__':
     main()
