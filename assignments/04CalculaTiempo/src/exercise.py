edbd734ccf105c21fc1bc97a9dfb6ef4361c3afe
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 12:06:18 -0500

    Update exercise.py

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -2,7 +2,12 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
-
+#100 Años
+edad=int(input('Dame tu edad: '))
+año=int(input('Dame el año actual: '))
+fechan=año-edad
+cumpleaños=fechan+100
+print('Cumples 100 años el año: %0.0f' %cumpleaños)
 
 if __name__ == '__main__':
     main()
