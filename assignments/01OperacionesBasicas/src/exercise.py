081661a8317f1174d93c3fa5b0f1b91f7d43e0e6
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 12:24:49 -0500

    Update exercise.py

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -2,6 +2,15 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+#Operaciones básicas
+número1=int(input('Dame un número: '))
+número2=int(input('Dame un número: '))
+suma=(número1+número2)
+resta=(número1-número2)
+multiplicación=(número1*número2)
+print('Suma: %0.0f' %suma)
+print('Resta: %0.0f' %resta)
+print('Multiplicación: %0.0f' %multiplicación)
 
 if __name__ == '__main__':
     main()
