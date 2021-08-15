2e6ec594a54a9e118ba2f20779910333ef216225
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 14:15:12 -0500

    Update exercise.py

--- assignments/11PEMDAS/src/exercise.py
+++ assignments/11PEMDAS/src/exercise.py
@@ -5,21 +5,20 @@ def main():
     #Fecha: Agosto 8 de 2019
     #Actividad Prioridad de Operaciones Básicas 
 
-    from math import sqrt
-    
-    oper1 = 2*(3/4) + 4*(2/3) - 3*(1/5) + 5*(1/2)
-    print(round(oper1,4))
-    oper2 = 2* sqrt(35**2) + 4*sqrt(36**3) - 6*sqrt(49**2)
-    print(round(oper2,4))
-    a = 4
-    b = 5
-    oper3 =(a**3 + 2*b**2) / (4*a) 
-    print(round(oper3,4))
-    oper4 = (2*(a+b)**2 + 4*(a-b)**2) / (a*b**2)
-    print(round(oper4,4))
-    oper5 = sqrt((a+b)**2 + 2**(a+b)) / (2*a +2*b)**2
-    print(round(oper5,4))
-
+#PEMDAS
+import math
+oper1=float(2*3/4+4*2/3-3*1/5+5*1/2)
+oper2=float(2*math.sqrt(35**2)+4*math.sqrt(36**3)-6*math.sqrt(49**2))
+a=4
+b=5
+oper3=float((a**3+2*b**2)/(4*a)) 
+oper4=float((2*(a+b)**2+4*(a-b)**2)/(a*b**2))
+oper5=float(math.sqrt((a+b)**2+2**(a+b))/(2*a+2*b)**2)
+print(round(oper1, 4))
+print(round(oper2, 4))
+print(round(oper3, 4))
+print(round(oper4, 4))
+print(round(oper5, 4))
 
 
 
