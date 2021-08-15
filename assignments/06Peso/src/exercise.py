da2ce128af5a640e5103e5c193c3ec650c7387cf
Jeronimo-Uribe <88981236+Jeronimo-Uribe@users.noreply.github.com>
2021-08-15 12:13:40 -0500

    Update exercise.py

--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,8 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-    
 
+   #Peso
+pesoi=float(input('Dame el peso inicial: '))
+pesof=float(input('Dame el peso final: '))
+meses=int(input('Dame la cantidad de meses: '))
+pesob=pesoi-pesof
+pesobm=pesob/meses
+print('Lo que debes bajar por mes es: %0.2f' %pesobm) 
 
 if __name__ == '__main__':
     main()
